1b61a88a7d2a3424fed422d061d3a4650e4788ea
xRosemary <2862656629@qq.com>
2023-10-06 16:52:49 +0800

    refactor: 移动ui绑定装饰器位置

---
+++ types/Decorator/UIDecorator.d.ts
@@ -0,0 +1,15 @@
+import { Action } from '../Framework/AE';
+export declare class BindUMG extends Action {
+    name: string;
+    widget: string;
+    action: string;
+    targetCtor: Function;
+    propertyKey: string;
+    constructor(name: string, widget: string, action: string, targetCtor: Function, propertyKey: string);
+}
+/**
+ * 监听UI事件
+ * @param path 表格路径
+ * @todo 想办法移到Decorator文件夹中
+ */
+export declare function ui(ctor: string, widget: string, action: string): (target: any, propertyKey: string, descriptor: PropertyDescriptor) => void;
--- types/Framework/UI/UISystem.d.ts
+++ types/Framework/UI/UISystem.d.ts
@@ -1,5 +1,6 @@
 import { System } from '../ECS';
-import { BindUMG, CloseUMG, OpenUMG } from './PublicAE';
+import { CloseUMG, OpenUMG } from './PublicAE';
+import { BindUMG } from '../../Decorator/UIDecorator';
 export declare class UISystem extends System {
     protected onOpenUMG(action: OpenUMG): void;
     protected onCloseUMG(action: CloseUMG): void;
